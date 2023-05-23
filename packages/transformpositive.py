e1870f13eeef4ca1d950ccb3fd0164ac7089b769
fleurvr <97021569+fleurvr@users.noreply.github.com>
2023-05-23 13:23:36 +0200

    Update transformpositive.py

--- packages/transformpositive.py
+++ packages/transformpositive.py
@@ -1,1 +1,16 @@
+'''
+Function to transform numbers in dataframe
+to positive numbers
 
+By Dr. Raymond Hoogendoorn
+Copyright 2021
+'''
+
+def transformPositive(df):
+    labels = df.columns.values
+    for label in labels:
+        try:
+            df[label] = abs(df[label])
+        except:
+            df[label] = df[label]
+    return df
